2313bacb8ad202e18697fd0fde248661512f5ca5
Michal Sukupcak <michal.sukupcak@gmail.com>
2015-08-11 09:05:32 +0200

    update

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@
 var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
         hideLoading();
-        init();
+        setHeights();
     }
 }, 10);
 
@@ -18,13 +18,30 @@ function hideLoading() {
     }, 200);
 }
 
-function init() {
+function setHeights() {
+
+    // Get heights
     var windowHeight = window.innerHeight;
     var headerHeight = document.getElementById('header').clientHeight;
-    var iframeHeight = windowHeight - headerHeight - 80;
+
+    // Cards
+    var homeCardTitleHeight = document.getElementsByClassName('homeCardTitle')[0].clientHeight;
+    var homeCardActionsHeight = document.getElementsByClassName('homeCardActions')[0].clientHeight;
+    var homeCardHeight = windowHeight - headerHeight - 16 - 16 - homeCardTitleHeight - 32 - homeCardActionsHeight - 16 - 6;
+    var homeCards = document.getElementsByClassName('homeCard');
+    for (var i = 0; i < homeCards.length; i++) {
+        homeCards[i].getElementsByClassName('homeCardContent')[0].style.height = homeCardHeight + 'px';
+    }
+
+    // Iframes
+    var iframeHeight = windowHeight - headerHeight - 16 - 16; // mdl-grid margin + padding
     var iframes = document.getElementsByClassName('iframe');
     for (var i = 0; i < iframes.length; i++) {
         iframes[i].height = iframeHeight;
     }
+
 }
 
+window.onresize = function () {
+    setHeights();
+}
